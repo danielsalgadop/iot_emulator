759721086b251e12f7cfbb97e5e3c4fcb236befa
danielsalgadop <daniel.salgado.p@gmail.com>
2018-11-29 20:14:14 +0100

    3 routes

--- src/Infrastructure/ThingController.php
+++ src/Infrastructure/ThingController.php
@@ -4,15 +4,28 @@
 namespace App\Infrastructure;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 //use Symfony\Component\Routing\Annotation\Route;
 
 class ThingController extends AbstractController
 {
 
-    public function index(Request $request): Response
+    public function index(): JsonResponse
     {
-        return new Response("listado de things");
+        return new JsonResponse(["key"=>"listado de things"]);
     }
+
+    public function search(int $id): JsonResponse
+    {
+        // find Thing
+        return new JsonResponse(["will search for this "=>$id]);
+    }
+
+    public function delete(int $id): JsonResponse
+    {
+        // find Thing
+        return new JsonResponse(["will delete this id"=>$id]);
+    }
+
 }
