1a62ea16457dbbd5a10273de69f1957e1239a815
danielsalgadop <daniel.salgado.p@gmail.com>
2018-12-23 14:28:38 +0100

    removed Entites, Respositories and Migrations

--- src/Domain/Repository/ActionRepository.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Domain\Repository;
-use App\Domain\Entity\Action;
-
-interface ActionRepository
-{
-    public function save(Action $action);
-}
--- src/Domain/Repository/ThingRepository.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Domain\Repository;
-use App\Domain\Entity\Thing;
-
-interface ThingRepository
-{
-    public function save(Thing $thing);
-}
