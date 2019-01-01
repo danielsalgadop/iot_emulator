6f7fad741f58bc5a7cd66d585694a094df2d3fce
danielsalgadop <daniel.salgado.p@gmail.com>
2019-01-01 23:05:20 +0100

    setting property value to ddbb

--- src/Application/CommandHandler/Thing/CreateThingHandler.php
+++ src/Application/CommandHandler/Thing/CreateThingHandler.php
@@ -24,7 +24,17 @@ class CreateThingHandler
 
         Thing::actionsAndPropertiesConcordance($objData->links->actions, $objData->links->properties);
 
-        foreach ($objData->links->actions as $actionName) {
+
+        for ($i = 0; $i < count($objData->links->actions); $i++) {
+                $action = new Action();
+                $property = new Property();
+                $property->setValue($objData->links->properties[$i]->{$objData->links->actions[$i]});  // madre mia, muy enrevesado!
+                $action->setProperty($property);
+                $action->setName($objData->links->actions[$i]);
+                $actionCollector[] = $action;
+            }
+
+            /*foreach ($objData->links->actions as $actionName) {
             $action = new Action();
             $property = new Property();
             $property->setValue($actionName);  // we asume properties born with action name
@@ -32,6 +42,7 @@ class CreateThingHandler
             $action->setName($actionName);
             $actionCollector[] = $action;
         }
+            */
 
         $thing = new Thing($objData->name,$objData->brand,$actionCollector);
         $this->thingRepository->save($thing);
