c8719c2c9246813aa00700c336f93f9c2b2e9464
danielsalgadop <daniel.salgado.p@gmail.com>
2019-01-27 20:19:00 +0100

    simple test for get_actions response

--- fixture/get_actions/get_actions.php
+++ fixture/get_actions/get_actions.php
@@ -2,13 +2,20 @@
 //error_reporting(E_STRICT);
 error_reporting(-1);
 
-define('ENDPOINT','http://localhost/3/actions');
+define('ENDPOINT','http://localhost');
+define('ACTION_PREFIX','hardCodedAction');
 
-function sendCurl(){
+if(!isset($argv[1])){
+    die('usage: need thing id (int)'.PHP_EOL);
+}
+
+$id = $argv[1];
+
+function sendCurl($id){
     $time = time();
 
 
-    $ch = curl_init(ENDPOINT);
+    $ch = curl_init(ENDPOINT.'/'.$id.'/actions');
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
     curl_setopt($ch, CURLOPT_PORT , 8000);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -22,7 +29,43 @@ function sendCurl(){
     $result = curl_exec($ch);
     file_put_contents("/tmp/get_actions.".$time.".html", __METHOD__ . ' ' . __LINE__ . PHP_EOL . var_export($result, true) . PHP_EOL, FILE_APPEND);
     var_dump($result);
+    return $result;
 }
 
 
-sendCurl();
+
+
+$json_result_as_array = json_decode(sendCurl($id),true);
+
+$expected_output = expectedOutput($id);
+//var_dump($expected_output);
+//var_dump($json_result_as_array);
+
+
+if($json_result_as_array !== $expected_output){
+    print 'ERROR!'.PHP_EOL;
+}
+else{
+    print 'TEST OK'.PHP_EOL;
+}
+
+function expectedOutput($id){
+    // building action
+    $expected_action_id = expectedActionIdStartingPoint($id);
+    $expected_data_structure=[];
+    for($i=0;$i<$id;$i++){
+
+        $action_incremental = $i+1;
+        $expected_data_structure[$i]['id'] = (int) $expected_action_id;
+        $expected_data_structure[$i]['name'] = ACTION_PREFIX.$action_incremental;
+        $expected_action_id++;
+    }
+    return $expected_data_structure;
+}
+
+function expectedActionIdStartingPoint($id){
+    $id--;
+    $number = (($id*$id)+$id)/2;
+    $number++;
+    return $number;
+}
