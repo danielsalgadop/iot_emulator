ce9b1ed595ad6995cbce080f1d83117c1b8a63a5
daniel salgado <daniel.salgado.p@gmail.com>
2019-05-26 22:13:19 +0200

    test hasActionsAndPropertiesConcordaneTest

---
+++ tests/Domain/Entity/Thing/hasActionsAndPropertiesConcordanceTest.php
@@ -0,0 +1,87 @@
+<?php
+// TODO: use phpunit dataproviders
+
+
+namespace App\Tests\Domain\Entity\Thing;
+
+
+use PHPUnit\Framework\TestCase;
+use App\Domain\Entity\Thing;
+
+class isIntegrityValidOnCreate extends TestCase
+{
+    public function testValidSerializedThingShouldReturnTrue()
+    {
+        foreach ($this->myArrayOfValidThingAsArrayProvider() as $validPropertiesAndActions) {
+
+            $actions = $validPropertiesAndActions['actions'];
+            $properties = $validPropertiesAndActions['properties'];
+
+            $this->assertTrue(Thing::hasActionsAndPropertiesConcordance($properties,$actions));
+        }
+    }
+
+    public function testInValidSerializedThingShouldReturnTrue()
+    {
+        foreach ($this->myArrayOfInValidThingAsArrayProvider() as $InValidPropertiesAndActions) {
+
+            $actions = $InValidPropertiesAndActions['actions'];
+            $properties = $InValidPropertiesAndActions['properties'];
+
+            $this->assertFalse(Thing::hasActionsAndPropertiesConcordance($properties,$actions));
+        }
+    }
+
+    public function myArrayOfValidThingAsArrayProvider()
+    {
+        return [
+            [
+                'properties' => [
+                    [
+                        'propertyName' => 'propertyValue'
+                    ],
+                ],
+                'actions' =>
+                    [
+                        'propertyName',
+                    ],
+            ],
+            [
+                'properties' => [],
+                'actions' => [],
+            ]
+        ];
+    }
+
+    public function myArrayOfInvalidThingAsArrayProvider()
+    {
+        return [
+            [ // StrangePropertyName (in actions)
+                'properties' => [
+
+                    [
+                        'propertyName' => 'propertyValue'
+                    ],
+                ],
+                'actions' =>
+                    [
+                        'StrangePropertyName',
+                    ],
+            ],
+            [ // StrangePropertyName (in properties)
+                'properties' => [
+
+                    [
+                        'StrangePropertyName' => 'propertyValue'
+                    ],
+                ],
+                'actions' =>
+                    [
+                        'propertyName',
+                    ],
+            ]
+
+
+        ];
+    }
+}
