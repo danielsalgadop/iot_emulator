3a52cd0f8fd960543b05212a249140244b27dbcd
daniel salgado <daniel.salgado.p@gmail.com>
2019-06-02 19:59:36 +0200

    PSR

--- src/Infrastructure/Thing/Serializer/ThingWithCredentials.php
+++ src/Infrastructure/Thing/Serializer/ThingWithCredentials.php
@@ -2,13 +2,13 @@
 
 
 namespace App\Infrastructure\Thing\Serializer;
+
 use App\Domain\Entity\Thing;
-use App\Infrastructure\Thing\Serializer\ThingWithoutCredentials;
-use App\Infrastructure\Thing\Serializer\ThingActions;
 
 class ThingWithCredentials
 {
-    public static function asObject(Thing $thing): \stdClass {
+    public static function asObject(Thing $thing): \stdClass
+    {
         $obj = new \stdClass();
         $obj->id = $thing->getId();
         $obj->name = $thing->getName();
